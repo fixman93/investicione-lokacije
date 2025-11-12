b3be89e62e6b770418bbb78079bd8bbb2ca7fce9
Boris (fixman93) <fixman93@users.noreply.github.com>
2025-11-12 14:05:21 +0100

    Update MapPage.tsx

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
